6b16cd40a9f075f815e112c464ba44efbb51d160
ksagar03 <71301717+ksagar03@users.noreply.github.com>
2023-07-27 15:49:57 +0000

    solved more problems on recursion

--- recursion.js
+++ recursion.js
@@ -26,8 +26,58 @@ and one more point to be remembered is to provide different input when function 
 
 // problem 2: write a recursive code for sumrange
 
-const sumrange = (n) => {
-  if (n == 1) return 1
-   return n + sumrange(n - 1)
+// const sumrange = (n) => {
+//   if (n == 1) return 1
+//    return n + sumrange(n - 1)
+// }
+// document.write(`sumrange: ${sumrange(4)} `)
+
+
+// problem 3 : factorial
+// const factorial = (n) => {
+//   if(n===0) return "invalid input"
+//   if (n===1) return 1
+//   return n * factorial(n-1)
+// }
+// document.write(`factorial = ${factorial(10)}`)
+
+
+// problem 4: to find odd numbers in an array using recursion
+
+const oddnumbers = (arr) =>{
+  let result = [] // if the value is odd then it will be stored inside this empty array
+  function findoddnumbers(input){
+    if(input.length === 0){
+      return;
+    }
+    if(input[0] %2 !== 0){
+      result.push(input[0])
+    }
+    findoddnumbers(input.slice(1))
+  }
+  findoddnumbers(arr)
+    return result
 }
-document.write(`sumrange: ${sumrange(4)} `)
+
+document.write(`odd numbers ${oddnumbers([1,2,3,4,5,6])} <br/>`)
+
+//  this above method is helpful when the value needs to be stored.
+// alternative method for the above problem
+// here nested function will not be used
+
+const alternatemethod = (arr) => {
+  let result =[]
+
+  if(arr.length ===0){
+    return 
+  } // base condition
+
+  if(arr[0]%2 !== 0){
+    result.push(arr[0])
+  }
+
+  result =result.concat(alternatemethod(arr.slice(1)))
+  return result
+}
+
+document.write(`alternate method where nested function is not used: ${alternatemethod([1,2,3,4,5,6])}`)
