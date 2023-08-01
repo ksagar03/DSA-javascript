54993a9a413f57942d28263721f8f94efa8cc28f
ksagar03 <71301717+ksagar03@users.noreply.github.com>
2023-08-01 15:35:02 +0000

    worked on string search naïve method which contains nested for loop.

--- searching_algorithm.js
+++ searching_algorithm.js
@@ -53,20 +53,42 @@ We have some built in algorithm like indexof which uses for loop to search the i
 // document.write(`${tofindstate(state,value)? `${value} is there in india` : `${value} is not there in india` }`)
 
 // 2. Binary search: It is faster than linear search algorithm but there is a catch in this the data or array  needs to be sorted. Here it eleminates halfs of the values based on the given input. It uses divide and conquer algorithm
+// time complexity => worst case => O(log n) best case O(1)
+// const binarysearch = (arr, value) => {
+//   let left = 0
+//   let right = arr.length - 1
+//   // console.log(left, right)
+//   while (left <= right) {
+//     mid = Math.floor((left + right) / 2)
+//     // console.log(mid)
+//     if (arr[mid] < value) {
+//       left = mid + 1
+//     } else if (arr[mid] > value) {
+//       right = mid - 1
+//     } else return mid
+//     // console.log(left, mid, right)
+//   }
 
-const binarysearch = (arr, value) => {
-  let left = 0
-  let right = arr.length - 1
-  console.log(left, right)
-  while (left <= right) {
-    mid = Math.floor((left + right) / 2)
-    if (arr[mid] < value) {
-      left = mid - 1
-    } else if (arr[mid] > value) {
-      right = mid + 1
-    } else return mid
-    console.log(left, mid, right)
+// }
+// console.log(`${binarysearch([1, 3, 4, 5, 7, 10,14], 10)}`)
+
+
+//eg: string search - naive method
+const stringsearch = (str, str1) => {
+  let count = 0
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < str1.length; j++) {
+      if (str[i + j] !== str1[j]) {
+        break
+      } // here i+j will help us iterate 1st string to match 2nd string, if the strings are not matching then it will break the inner loop and start from the first 
+      if (j == str1.length - 1) {
+        count++
+        // if j reaches to last string of str1 without breaking the innerloop then it will increment the count by one
+      }
+    }
   }
+  return count
 
 }
-console.log(`${binarysearch([1, 3, 4, 5, 7, 10], 3)}`)
+
+document.write(`number of string matches: ${stringsearch("hello llo", "ll")}`)
