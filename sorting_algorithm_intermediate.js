ed3de990aa959b87a4520721b7ad59ba746d106e
ksagar03 <71301717+ksagar03@users.noreply.github.com>
2023-08-11 10:33:43 +0000

    Learnt about unique sorting algorithm which does not work  number comparison.

--- sorting_algorithm_intermediate.js
+++ sorting_algorithm_intermediate.js
@@ -43,7 +43,7 @@ const merge_sort = (arr) => {
 
 }
 array = [10, 23, 13, 3, 4, 87]
-document.write(`MERGE SORT <br/> merge sort working <br/> <img class="merge_img" src="merge_sort_eg.jpg"> <br/>Given array =[${array}] <br/> merge sort solution [${merge_sort(array)}] <br/><br/>`)
+document.write(`MERGE SORT <br/> merge sort working <br/> <img class="image" src="merge_sort_eg.jpg"> <br/>Given array =[${array}] <br/> merge sort solution [${merge_sort(array)}] <br/><br/>`)
 
 merge_sort(array)
 //  this "merge_sort_eg will help us to merge the values of two array"
@@ -113,3 +113,72 @@ const quicksort = (arr, start = 0, end = arr.length - 1) => {
   return arr
 }
 console.log(quicksort([14, 2, 44, 21, 23, 11, 6]))
+document.write(`Quick sort time complexity <br/> <img class="image" src="Quick_sort_time_complexity.PNG"> <br/> The worst case is when the array is already sorted and quick sort algorithm is applied on it <br/>`)
+
+/* Radix sort::
+this are the special type of sorting algorithm which does not perform comparison to provide us the result(like other algorithm which I learnt above)
+Radix sort works on lists of numbers
+this algorithm only works on numbers.(strings can not be sorted using this algorithm )
+From the given array it starts arranging the value in zero, tens, hundreds, thousands and arranges it in the assending order
+
+*/
+
+
+document.write(`RADIX sort <br/> eg :[1556, 4, 3556, 593, 408, 4386, 902, 7, 8157, 86, 9637, 29] <br/> <img class="image" src="radix_sort_eg_starting_phase.PNG" />
+<br/> [902, 593, 4, 1556, 3556, 4386, 86, 7, 8157, 9637, 408, 29]
+<br/> after sorting everthing it will look like this
+<img class="image" src="radix_sort_eg.PNG" />
+<br/> for more information check this
+<a href="https://cs.slides.com/colt_steele/intermediate-sorting-algorithms#/30/0/2"> LINK</a>
+<br/><br/>
+Radix sort working example: <br/>
+given array :[1556, 4, 3556, 593, 408, 4386, 902, 7, 8157, 86, 9637, 29] <br/>`)
+
+
+// algorithm
+// first we need write helper function to provide a ones,tens,hundreds.... places eg: getdigit(3429,2) => results is 4 (from the last in 3rd place is "4" ) 
+// 1st helper function
+const getdigit = (num, place) => {
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10
+}
+// here Math.abs will convert -ve numbers into positive
+
+// console.log(getdigit(3429,0))
+// 2nd herlper function to find numbers of digits in a number
+const digitcount = (num) => {
+  if (num === 0) {
+    return 1
+    // this if condition is used because we are using log to count the digit log10(0) is infinity
+
+  }
+  return Math.floor(Math.log10(Math.abs(num))) + 1
+}
+// console.log(digitcount(3429))
+
+// 3rd helper function to find maxdigitcount of a number in the given list of the number
+
+const maxdigitcount = (numarr) => {
+  let maxdigit = 0
+  for (let i of numarr) {
+    maxdigit = Math.max(maxdigit, digitcount(i))
+  }
+  return maxdigit
+}
+// console.log(maxdigitcount([902, 593, 4, 155]))
+
+const Radixsort = (num) => {
+  let maxdigits = maxdigitcount(num)
+  for (let i = 0; i < maxdigits; i++) {
+    let digitbuckets = Array.from({ length: 10 }, () => []) // this line will create 10 empty arrays or we can create those arrays manually [[],[]....]
+    for (let j = 0; j < num.length; j++) {
+      let getdigits = getdigit(num[j], i)
+      digitbuckets[getdigits].push(num[j])
+    }
+    // console.log(digitbuckets)
+    num = [].concat(...digitbuckets)
+    document.write(`[${num}] <br/>`)
+  }
+  return num
+}
+
+console.log(Radixsort([1556, 4, 3556, 593, 408, 4386, 902, 7, 8157, 86, 9637, 29]))
