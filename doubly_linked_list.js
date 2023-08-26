d586f3d8685172863831aedb8772c766762856af
ksagar03 <71301717+ksagar03@users.noreply.github.com>
2023-08-26 10:55:45 +0000

    Implemented get, set, insert, remove methods on doubly linked list

--- doubly_linked_list.js
+++ doubly_linked_list.js
@@ -9,7 +9,7 @@ The linked list contains <u>head</u> and <u>tail</u> which points towards start 
 
 <br/>
 In linked list insertion and deletion of values was easy but accessing values randomlly is not so easy. <br/>
-<b>Time Complexity </b>: <br/> For insertion it is O(1) (but in array it is O(n) ) <br/>for removal it is O(1)(this is for removing the data from the start) or O(n) (when the data is removed from the last then we need to iterate over the list) <br/> for searching and accessing the data it is O(n)  `)
+<b>Time Complexity </b>: <br/> For insertion it is O(1) (but in array it is O(n) ) <br/>for removal it is O(1)(this is for removing the data from the start and from the end) or O(n/2) (when the data is removed from the user provided index) <br/> for searching and accessing the data it is O(n)  or O(n/2) (it changes based on the user provided index)<br/> <b>example where the doubly linked list is used: In browseing we can go to previous page and next page </b> `)
 
 class Node {
   constructor(val) {
@@ -71,25 +71,91 @@ class doubly_linked_list {
     this.length--
     return currentHead.value
   }
-  unshift(val){
+  unshift(val) {
     // to add element at the start
     let newNode = new Node(val)
-    if(!this.head){
+    if (!this.head) {
       this.head = newNode
       this.tail = this.head
-    }else{
+    } else {
       newNode.prev = null
       newNode.next = this.head
       this.head.prev = newNode
       this.head = newNode
     }
     this.length++
-    return this 
+    return this
   }
+  get(position) {
+    if (position < 0 || position > this.length) {
+      return null
+    }
+    else if (position >= (this.length / 2)) {
+      console.log("working from the last")
+      let positionvalue = this.tail
+      for (let i = this.length - 1; i >= position; i--) {
+        positionvalue = positionvalue.prev
+      }
+      return positionvalue
+    } else {
+      console.log("working from the first")
+      let positionvalue = this.head
+      for (let i = 1; i < position; i++) {
+        positionvalue = positionvalue.next
+      }
+      return positionvalue
+    }
+  }
+  set(val, position) {
+    let getposition = this.get(position)
+    if (getposition) {
+      getposition.value = val
+      return getposition.value
+    }
+    return "undefined position"
+  }
+  insert(val, position) {
+    if (position === 0 || position === 1) return !!this.unshift(val)
+    if (position === this.length) return !!this.push(val)
+    let getposition = this.get(position)
+    if (getposition) {
+      let NewNode = new Node(val)
+      let currentposition = getposition
+      let prevposition = getposition.prev
+      NewNode.prev = prevposition
+      NewNode.next = currentposition
+      prevposition.next = NewNode
+      currentposition.prev = NewNode
+      this.length++
+      return true
+    }
+    return "undefined position"
+  }
+remove(position){
+  if (position ===0 || position === 1) return !!this.shift()
+  if (position === this.length) return !!this.pop()
+  let currentNode = this.get(position)
+  if (currentNode){
+    let prevNode = currentNode.prev
+    let nextNode = currentNode.next
+    prevNode.next = nextNode
+    nextNode.prev = prevNode
+    currentNode.prev = null
+    currentNode.next = null
+    this.length--
+    return true 
+  }else return false
+}
 }
 
 let list = new doubly_linked_list()
 list.push(1)
 list.push(2)
-// list.push(3)
-// list.push(4)
+list.push(3)
+list.push(4)
+list.push(5)
+list.push(6)
+list.push(7)
+list.push(8)
+list.push(9)
+
