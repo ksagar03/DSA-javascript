cd770d612ec903fec09a9cc705fe6cf791636f51
ksagar03 <71301717+ksagar03@users.noreply.github.com>
2023-09-13 15:14:45 +0000

    Started Learning Graphs

---
+++ Graphs.js
@@ -0,0 +1,6 @@
+document.write(`<h2>Graphs</h2>Graphs are very useful data structure,<li>It is used in almost all the places from social media(instagram reels, adds recommandation) to streeming websites(movie recommandation) </li>
+<img class="image" src="images/UseOfGraphs.png" />
+
+<b>Defination</b> It is a collection of nodes and connection between the nodes. 
+<br/>
+<img class="image" src="images/Graphs_eg.png" />`)
